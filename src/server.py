d7a7e1ceac3333a864f7c827c78eb6a5c5f418d8
Bogachevv <dog67688@gmail.com>
2023-11-29 21:12:18 +0300

    Re-write server skeleton, using JS and AJAX

--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
-from flask import render_template, redirect, url_for
+import time
+
+from flask import Flask, request, Response
+from flask import render_template, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder='HTML')
@@ -8,53 +10,47 @@ app.config['SECRET_KEY'] = 'hello'
 Bootstrap(app)
 
 
-@app.route('/')
-def index():
-    return 'Index'
+MODELS = {}
 
 
-@app.route('/api/select', methods=['GET', 'POST'])
-def model_selection():
+@app.route('/', methods=['GET', 'POST'])
+def model_settings():
     if request.method == 'GET':
-        return render_template('model_selection.html')
+        return render_template('index.html')
 
     # serving POST request
     model_type = request.form['model_selection']
-
-    return redirect(url_for('model_params', model_type=model_type))
-
-
-@app.route('/api/params', methods=['GET', 'POST'])
-def model_params():
-    if request.method == 'GET':
-        model_type = request.args.get('model_type')
-
-        return render_template('model_params.html', model_type=model_type)
-
-    # serving POST request
-
-    model_type = request.args.get('model_type')
     n_estimators = request.form['n_estimators']
-    max_depth = request.form['max_depth']
     max_depth_on = ('max_depth_on' in request.form)
-    feature_subsample_size = request.form['feature_subsample_size']
+    max_depth = request.form['max_depth'] if max_depth_on else None
     feature_subsample_size_auto = ('feature_subsample_size_auto' in request.form)
+    feature_subsample_size = request.form['feature_subsample_size'] if not feature_subsample_size_auto else None
 
-    print(model_type, n_estimators, max_depth, max_depth_on, feature_subsample_size, feature_subsample_size_auto, sep='\n')
+    print(model_type, n_estimators, max_depth, max_depth_on, feature_subsample_size, feature_subsample_size_auto,
+          sep='\n')
 
-    # return redirect(url_for('index'))
-    return redirect(url_for(
-        endpoint='select_dataset',
-        model_type=model_type,
-        n_estimators=n_estimators,
-        max_depth=max_depth,
-        max_depth_on=max_depth_on,
-        feature_subsample_size=feature_subsample_size,
-        feature_subsample_size_auto=feature_subsample_size_auto
-    ))
+    print(request.form)
+
+    return redirect(url_for('model', model_no=123))
 
 
-@app.route('/api/dataset', methods=['GET', 'POST'])
-def select_dataset():
-    return render_template('select_dataset.html')
+@app.route('/model/gui', methods=['GET'])
+def model():
+    return render_template('model.html')
 
+
+@app.route('/model/fit', methods=['GET'])
+def model_fit():
+    model_no = request.args.get('model_no')
+    MODELS[model_no] = 0
+
+    time.sleep(5)
+
+    return 'OK', 200
+
+
+@app.route('/model/status', methods=['GET'])
+def get_model_status():
+    model_no = request.args.get('model_no')
+    xml = 'fit' if model_no in MODELS else 'not_fit'
+    return Response(xml, mimetype='text/xml'), 200
